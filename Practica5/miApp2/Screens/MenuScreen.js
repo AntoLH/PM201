d12aa932bc2ec8a6d86ef943d17e35637c05d175
AntoLH <123048409@upq.edu.mx>
2026-07-01 12:29:12 -0600

    Practica 15

--- Practica5/miApp2/Screens/MenuScreen.js
+++ Practica5/miApp2/Screens/MenuScreen.js
@@ -1,56 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React, { useState } from 'react';
-import TarjetasScreen from './TarjetasScreen';
-import Componente1 from './Componente1';
-import FlatListScreen from './Practica4/FlatListScreen';
-import SectionListScreen from './Practica4/SectionListScreen';
-import Practicagena from './Practicagena';
-import PressableScreen from './PressableScreen';
-import SwitchScreen from './switchScreen';
-import { Componente4_0 } from './componente4_0';
-import ComponenteAlert from './componentealerta';
+/* Zona 1: Improtaciones de archivos y componentes */
 
-export default function App() {
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import React,{Component, useEffect, useState} from 'react';
+import TarjetasScreen from '../Screens/TarjetasScreen';
+import PressableScreen from './PressableScreen';
+import {ImagenFondo} from './ImagenFondo';
+import { SplashScreen } from './SplashScreen';
+import {Componente1} from './Componente1';
+import { Home } from './Home';
+import SwitchScreen from './SwitchScreen';
+import { Componente4_0 } from './Componente4_0';
+import {Alerta} from './Alerta'
+import { FlatListScreen } from './FlatListScreen';
+import { SectionListScreen } from './SectionListScreen';
+
+/* Zona 2: Main - Componentes del Screen */
+export default function MenuScreen() {
     const [screen, setScreen] = useState('menu');
-    
-    switch(screen){
-        case 'tarjetas':
+
+    useEffect(() => {
+        if (screen === 'splashScreen') {
+            const timer = setTimeout(() => {
+                setScreen('home');
+            }, 6000);
+            return () => clearTimeout(timer);
+        }
+    }, [screen]);
+
+    switch (screen) {
+        case 'tarjetas': 
             return <TarjetasScreen/>;
-        case 'Componente1':
+        case 'componente1':
             return <Componente1/>;
-        case 'flatlist':
-            return <FlatListScreen/>;
-        case 'SectionList':
-            return <SectionListScreen/>;
-        case 'practicagena':
-            return <Practicagena/>;
-        case 'pressable':
+        case 'pressableScreen': 
             return <PressableScreen/>;
         case 'switch':
             return <SwitchScreen/>;
-        case 'componente4_0':
-            return <Componente4_0/>;
-        case 'alerta':
-            return <ComponenteAlert/>;
+        case 'input':
+            return <Componente4_0/>
+        case 'alert':
+            return <Alerta/>
+        case 'flatList':
+            return <FlatListScreen/>
+        case 'sectionList':
+            return <SectionListScreen/>
+        case 'imagenFondo':
+            return <ImagenFondo style={styles.container}/>;
+        case 'home':
+            return <Home/>;
+        case 'splashScreen':
+            return <SplashScreen/>;
         case 'menu':
-        default:
-            return (
-                <View style={styles.container}>
-                    <Text style={styles.titulo}> Menú de prácticas </Text>
-                    <Button title="Practica tarjetas" onPress={() => setScreen('tarjetas')}/>
-                    <Button title="Practica Componente1" onPress={() => setScreen('Componente1')}/>
-                    <Button title="Practica FlatList" onPress={() => setScreen('flatlist')}/>
-                    <Button title="Practica SectionList" onPress={() => setScreen('SectionList')}/>
-                    <Button title="Practica Gena" onPress={() => setScreen('practicagena')}/>
-                    <Button title="Practica Pressable" onPress={() => setScreen('pressable')}/>
-                    <Button title="Practica Switch" onPress={() => setScreen('switch')}/>
-                    <Button title="Practica TextInputs" onPress={() => setScreen('componente4_0')}/>
-                    <Button title="Practica Alertas" onPress={() => setScreen('alerta')}/>
-                </View>
-            );
-    }
-}
+            default:
+                return (
+                    <View style={styles.container}>
+                        <Text>Menú de prácticas</Text>
+                        <Button title='Práctica Tarjetas' onPress={ () => setScreen('tarjetas') }/>
+
+                        <Button title="Componente 1" onPress={() => setScreen('componente1')}/>
+
+                        <Button title='PressableScreen' onPress={() => setScreen('pressableScreen') }/>
+
+                        <Button title='Switch' onPress={() => setScreen('switch')}/>
+
+                        <Button title='Input' onPress={() => setScreen('input')}/>
+
+                        <Button title='Alerta' onPress={() => setScreen('alert')}/>
+
+                        <Button title='Imagen Bg' onPress={() => setScreen('imagenFondo')}/>
+
+                        <Button title='Splash' onPress={() => setScreen('splashScreen')}/>
+                        
+                        <Button title='Flat List' onPress={() => setScreen('flatList')}/>
+
+                        <Button title='Section List' onPress={() => setScreen('sectionList')}/>
+                    </View>
+                );//Return
+    }//Switch
+}//Función
+
+/* Zona 3: Estilos y posicionamiento */
 
 const styles = StyleSheet.create({
   container: {
